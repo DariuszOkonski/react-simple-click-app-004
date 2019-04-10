983031fa0bc135545bd100b19f075da7037663dc
Dariusz Okonski <darek200180@gmail.com>
2019-04-10 23:01:38 +0100

    Updates

---
+++ precache-manifest.5a2b30d0ad61edc6a76bf5d4942aa7d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8587a0fd494e163058d0",
+    "url": "/react-simple-click-app-004/static/js/runtime~main.d409ee33.js"
+  },
+  {
+    "revision": "05eec4c1fec816620774",
+    "url": "/react-simple-click-app-004/static/js/main.ed8796f6.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-simple-click-app-004/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "05eec4c1fec816620774",
+    "url": "/react-simple-click-app-004/static/css/main.1d3130d1.chunk.css"
+  },
+  {
+    "revision": "1e49203cb49c38e3274ea521727ae744",
+    "url": "/react-simple-click-app-004/index.html"
+  }
+];
